6fc0c71ce7ce23fbdf0f2315168c77ed2e130067
Tim Berlin <49077932+tberlin-om@users.noreply.github.com>
2023-10-21 13:28:01 +0200

    Update hreflang-sitemap.py

--- hreflang-sitemap.py
+++ hreflang-sitemap.py
@@ -22,7 +22,8 @@ output_folder = 'output'
 output_base_name = 'hreflang_sitemap'
 output_counter = 1
 max_urls_per_sitemap = 20000
-urlset = ET.Element('urlset', xmlns='https://www.sitemaps.org/schemas/sitemap/0.9')
+urlset = ET.Element('urlset', xmlns='https://www.sitemaps.org/schemas/sitemap/0.9', 
+                    attrib={'xmlns:xhtml': 'http://www.w3.org/1999/xhtml'})
 
 url_count = 0
 
@@ -31,44 +32,38 @@ with open(csv_file_path, 'r', newline='') as csvfile:
     languages = next(csv_reader)
     
     for row in csv_reader:
-        url = row[0]
-        url_element = ET.SubElement(urlset, 'url')
-        loc_element = ET.SubElement(url_element, 'loc')
-        loc_element.text = url
+        url_element = ET.Element('url')
+        primary_url = row[0]
+        primary_lang = languages[0]
         
-        hreflang_entries = 0
+        # Check if there's at least one alternate URL
+        has_alternate = any(row[1:])
+        
+        if not has_alternate:
+            continue
+        
+        loc_element = ET.SubElement(url_element, 'loc')
+        loc_element.text = primary_url
+        
+        primary_hreflang = ET.SubElement(url_element, 'xhtml:link', hreflang=primary_lang, rel='alternate', href=primary_url)
+        
         for i, lang_code in enumerate(languages[1:]):
             hreflang_url = row[i + 1]
             if hreflang_url and hreflang_url.strip():
-                hreflang_element = ET.SubElement(url_element, 'xhtml:link', hreflang=lang_code, rel='alternate')
-                hreflang_element.set('href', hreflang_url)
-                hreflang_entries += 1
-                
-        if hreflang_entries > 0:
+                hreflang_element = ET.SubElement(url_element, 'xhtml:link', hreflang=lang_code, rel='alternate', href=hreflang_url)
+
+        urlset.append(url_element)
+        url_count += 1
+        if url_count >= max_urls_per_sitemap:
             pretty_print(urlset)
             
             output_file = f'{output_folder}/{output_base_name}_{output_counter}.xml'
             tree = ET.ElementTree(urlset)
             tree.write(output_file, encoding='utf-8', xml_declaration=True)
             
-            sitemap_file = open(output_file, 'r', encoding='utf-8')
-            sitemap_contents = sitemap_file.read()
-            sitemap_file.close()
-            
-            sitemap_contents = '<?xml version="1.0" encoding="UTF-8"?>\n' + sitemap_contents
-            sitemap_contents = '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" ' \
-                               'xmlns:xhtml="https://www.w3.org/1999/xhtml">\n' + sitemap_contents
-            sitemap_contents += '</urlset>\n'
-            
-            sitemap_file = open(output_file, 'w', encoding='utf-8')
-            sitemap_file.write(sitemap_contents)
-            sitemap_file.close()
-            
-            url_count += 1
-            if url_count >= max_urls_per_sitemap:
-                url_count = 0
-                output_counter += 1
-                urlset.clear()
+            url_count = 0
+            output_counter += 1
+            urlset.clear()
 
 if url_count > 0:
     pretty_print(urlset)
