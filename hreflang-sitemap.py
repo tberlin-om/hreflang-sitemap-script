8edc238331a28fd17844e702c0232328b57776f3
Tim Berlin <49077932+tberlin-om@users.noreply.github.com>
2023-10-21 13:38:21 +0200

    Update hreflang-sitemap.py

--- hreflang-sitemap.py
+++ hreflang-sitemap.py
@@ -1,75 +1,69 @@
 import csv
-import xml.etree.ElementTree as ET
-import os
+from xml.etree.ElementTree import Element, SubElement, ElementTree, tostring
+from xml.dom import minidom
 
-def pretty_print(elem, level=0):
-    i = "\n" + level*"  "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            pretty_print(elem, level+1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
+def prettify(elem):
+    rough_string = tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+def split_large_sitemaps(root, threshold):
+    count = len(root.findall('url'))
+    sitemaps = []
+
+    if count > threshold:
+        while len(root):
+            new_root = Element('urlset', {
+                'xmlns': 'https://www.sitemaps.org/schemas/sitemap/0.9',
+                'xmlns:xhtml': 'https://www.w3.org/1999/xhtml'
+            })
+
+            for _ in range(threshold):
+                if len(root):
+                    url_element = root[0]
+                    root.remove(url_element)
+                    new_root.append(url_element)
+
+            sitemaps.append(new_root)
+
+        return sitemaps
     else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
+        return [root]
 
-csv_file_path = 'hreflang-data.csv'
-output_folder = 'output'
-output_base_name = 'hreflang_sitemap'
-output_counter = 1
-max_urls_per_sitemap = 20000
-urlset = ET.Element('urlset', xmlns='https://www.sitemaps.org/schemas/sitemap/0.9', 
-                    attrib={'xmlns:xhtml': 'https://www.w3.org/1999/xhtml'})
+def generate_sitemap_from_csv(csv_file, threshold=20000):
+    urlset = Element('urlset', {
+        'xmlns': 'https://www.sitemaps.org/schemas/sitemap/0.9',
+        'xmlns:xhtml': 'https://www.w3.org/1999/xhtml'
+    })
 
-url_count = 0
+    with open(csv_file, mode='r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        headers = next(reader)
 
-with open(csv_file_path, 'r', newline='') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    languages = next(csv_reader)
-    
-    for row in csv_reader:
-        url_element = ET.Element('url')
-        primary_url = row[0]
-        primary_lang = languages[0]
-        
-        # Check if there's at least one alternate URL
-        has_alternate = any(row[1:])
-        
-        if not has_alternate:
-            continue
-        
-        loc_element = ET.SubElement(url_element, 'loc')
-        loc_element.text = primary_url
-        
-        primary_hreflang = ET.SubElement(url_element, 'xhtml:link', hreflang=primary_lang, rel='alternate', href=primary_url)
-        
-        for i, lang_code in enumerate(languages[1:]):
-            hreflang_url = row[i + 1]
-            if hreflang_url and hreflang_url.strip():
-                hreflang_element = ET.SubElement(url_element, 'xhtml:link', hreflang=lang_code, rel='alternate', href=hreflang_url)
+        for row in reader:
+            for idx, url in enumerate(row):
+                if url:
+                    url_element = SubElement(urlset, 'url')
+                    loc_element = SubElement(url_element, 'loc')
+                    loc_element.text = url
 
-        urlset.append(url_element)
-        url_count += 1
-        if url_count >= max_urls_per_sitemap:
-            pretty_print(urlset)
-            
-            output_file = f'{output_folder}/{output_base_name}_{output_counter}.xml'
-            tree = ET.ElementTree(urlset)
-            tree.write(output_file, encoding='utf-8', xml_declaration=True)
-            
-            url_count = 0
-            output_counter += 1
-            urlset.clear()
+                    for sub_idx, alternate_url in enumerate(row):
+                        if alternate_url and idx != sub_idx:
+                            xhtml_element = SubElement(url_element, 'xhtml:link', {
+                                'rel': 'alternate',
+                                'hreflang': headers[sub_idx],
+                                'href': alternate_url
+                            })
 
-if url_count > 0:
-    pretty_print(urlset)
-    
-    output_file = f'{output_folder}/{output_base_name}_{output_counter}.xml'
-    tree = ET.ElementTree(urlset)
-    tree.write(output_file, encoding='utf-8', xml_declaration=True)
+    sitemaps = split_large_sitemaps(urlset, threshold)
 
-print(f'Die hreflang-Sitemaps wurden im Ordner "{output_folder}" erstellt.')
+    return sitemaps
+
+sitemaps = generate_sitemap_from_csv('input.csv')
+
+for idx, sitemap in enumerate(sitemaps):
+    sitemap_xml = prettify(sitemap)
+    with open(f'sitemap_{idx + 1}.xml', 'w', encoding='utf-8') as file:
+        file.write(sitemap_xml)
+
+print(f"{len(sitemaps)} Sitemaps wurden generiert!")
